e8734ba0421729c0cabf121ea5e210dccb1bde95
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-10-27 17:37:09 -0400

    Fake change

--- server/render.jsx
+++ server/render.jsx
@@ -5,5 +5,8 @@ import render from 'preact-render-to-string';
 import Message from './message';
 //
 export default (options, markup) => {
+    if (true) {
+        console.log('no real change to force CI run');
+    }
     return render(<Message options={options} markup={markup} locale={markup.meta.offerCountry} />);
 };
