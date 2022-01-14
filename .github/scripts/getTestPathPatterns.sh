3de467647606ce41c295222ce2619849494ffa05
Merlin (they/them) Patterson <421960+merlinpatt@users.noreply.github.com>
2022-01-14 11:21:50 -0500

    fix script

--- .github/scripts/getTestPathPatterns.sh
+++ .github/scripts/getTestPathPatterns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # lists all functional test paths and then removes text leaving only {locale}/{type}/{subtype}
-validTestPaths=$(ls ../../tests/functional/spec/*/*/*.test.js | sed -E 's/(.*spec\/|.test.js)//g')
+validTestPaths=$(ls ./tests/functional/spec/*/*/*.test.js | sed -E 's/(.*spec\/|.test.js)//g')
 # transforms the list into a comma separated json array
 testPathPatterns="[ \"$(echo $validTestPaths | tr '\n' ',' | sed 's/,$//' | sed 's/,/","/g' )\" ]"
 
