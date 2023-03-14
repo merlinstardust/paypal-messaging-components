40fb58c8d68a1edd29e8153ab1d4f98ef697c153
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2023-03-14 15:17:09 -0400

    Create getVersions

--- scripts/rollback-release.sh
+++ scripts/rollback-release.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-lastTag=$(git tag | tail -1)
+# upstreamUrl="https://github.com/paypal/paypal-messaging-components.git"
 
-read -p "Do you wish to roll back to the previous release of $lastTag? (Y or y)" -n 1 -r
-echo
+# git fetch --tags $upstreamUrl
+
+function getVersions() {
+  git tag | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort | awk '{print $4}'
+}
+
+# versions=$(git tag | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort | awk '{print $4}')
+latestVersion="$(getVersions | tail -1)"
+previousVersion=$(getVersions | tail -2 | head -1)
+
+echo $latestVersion
+echo "-----"
+echo $previousVersion
+echo "-----"
+
+read -p "Do you wish to roll back from $latestVersion to $previousVersion? (Y or y) " -n 1 -r
+echo ""
+echo ""
 
 if [[ $REPLY =~ ^[Yy]$ ]];
 then
-  git checkout release
-  git reset --hard $lastTag
+  # git checkout release
+  # git reset --hard $lastTag
 
   echo "Local release branch rolled back"
-  read -p "Are you certain you wish to revert to the release of $lastTag? (Y or y)" -n 1 -r secondConfirm
+  read -p "Are you certain you wish to revert $latestVersion to $previousVersion? (Y or y) " -n 1 -r secondConfirm
+  echo ""
+  echo ""
 
-  if [[ $REPLY =~ ^[Yy]$ ]];
+  if [[ $secondConfirm =~ ^[Yy]$ ]];
   then
-    # git push -f https://github.com/paypal/paypal-messaging-components.git
+    echo "Force pushing $previousVersion to overwrite $latestVersion"
+    # git push -f $upstreamUrl
   else
     echo "Skipping force push"
   fi
